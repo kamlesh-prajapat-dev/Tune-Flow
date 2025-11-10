005654a11b3621d8f440bacedf6b27981471e9cf
Kamlesh Prajapat <kamlesh-prajapat-dev@gmail.com>
2025-11-10 22:56:29 +0530

    Add some changes

--- composeApp/src/commonMain/kotlin/org/example/project/data/remote/api/JamendoApi.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/data/remote/api/JamendoApi.kt
@@ -9,7 +9,7 @@ import org.example.project.utils.ApiResult
 
 object JamendoApi {
     private const val BASE_URL = "https://api.jamendo.com/v3.0"
-    private const val CLIENT_ID = "792cc03a"
+    private const val CLIENT_ID = "Your Api"
     private val client get() = HttpClientProvider.client
     suspend fun getTracks(limit: Int = 20): ApiResult<JamendoResponse> = safeApiCall {
 
